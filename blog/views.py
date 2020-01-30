d123f28b6a527e6d4cdc6bbddfdeccd338d40889
anujchourange <chourangeanuj@gmail.com>
2020-01-30 22:15:22 +0530

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Anuj C.',
-        'title': 'Blog-Post',
-        'content': 'First post content',
-        'date_posted': 'August 28 2020'
-    },
-    {
-        'author': 'New Me',
-        'title': 'Blog-Post',
-        'content': 'First post content',
-        'date_posted': 'August 28 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            user_data = User(username=username, password=password, email=email)
+            user_data.save()
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
